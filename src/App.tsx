5986eda08940c4ae314cd502566457f7385044e4
ayencha <ayl0ra@outlook.com>
2025-06-06 17:50:41 +0200

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { LanguageProvider } from './contexts/LanguageContext';
 function App() {
   return (
     <LanguageProvider>
-      <div className="min-h-screen bg-black">
+      <div className="min-h-screen bg-'https://www.kali.org/wallpapers/images/2024/kali-ferrofluid.jpg'>
         <Terminal />
       </div>
     </LanguageProvider>
